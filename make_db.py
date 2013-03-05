3c91828a9a4db09b54c8237a75fa77eb0335e64b
Aman Kapur <amankapur91@gmail.com>
2013-03-05 01:09:42 -0500

    db freeze

--- make_db.py
+++ make_db.py
@@ -14,18 +14,19 @@ for file_name in os.listdir(path):
 	print "SONG " + file_name
 	if file_name.endswith('.mp3'):
 		file_path = './audio/' + file_name
-		song = track.track_from_filename(file_path, force_upload=True)
+		song = track.track_from_filename(file_path, force_upload=False)
 
 		params = {}
 		params['song_name'] = file_name
 		params['id'] = song.id
 		for segment in  song.segments:
-			params['timbre'] = segment.timbre
-			params['pitch'] = segment.pitch
-			params['duration'] = segment.duration
-			params['loudness_start'] = segment.loudness_start
-			params['loudness_max_time'] = segment.loudness_max_time
-			params['loudness_max'] = segment.loudness_max
+			# print segment.keys()
+			params['timbre'] = segment['timbre']
+			params['pitches'] = segment['pitches']
+			params['duration'] = segment['duration']
+			params['loudness_start'] = segment['loudness_start']
+			params['loudness_max_time'] = segment['loudness_max_time']
+			params['loudness_max'] = segment['loudness_max']
 
 			requests.post(mongo_url, data=json.dumps(params), headers= headers)
 
